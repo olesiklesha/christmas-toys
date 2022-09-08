2d3f510ef39a1e37956dfe93fc05b727ed0b3904
olesiklesha <olesik736@gmail.com>
2022-09-08 11:32:05 +0300

    feat: add base structure to the thee page

---
+++ src/pages/Tree/styles.ts
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+import { Wrapper } from '../../common-styled-components';
+
+export const Container = styled(Wrapper)`
+  display: flex;
+  flex-direction: row;
+`;
