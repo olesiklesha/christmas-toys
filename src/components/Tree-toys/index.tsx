1f3205d5e1a8066a653a52928f90ae07a925d8e0
olesiklesha <olesik736@gmail.com>
2022-09-12 11:47:43 +0300

    feat: add logic for getting toys

--- src/components/Tree-toys/index.tsx
+++ src/components/Tree-toys/index.tsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import { Container, ToyContainer } from './styles';
 import { BlockTitle } from '../Filters-section/styles';
+import { useAppSelector } from '../../hooks';
+import toys from '../../data/toys-data.json';
+import { IToy } from '../../models';
+
+const toysData = toys as IToy[];
 
 const TreeToys = () => {
+  const { selected } = useAppSelector((state) => state.selectedToysSlice);
+  const data = toysData.filter((el) => {
+    if (selected.length > 0 && selected.includes(el.num)) {
+      return el;
+    } else {
+      if (el.favorite) return el;
+    }
+  });
+
   return (
     <Container>
       <BlockTitle>Toys</BlockTitle>
       <ToyContainer>
-        <span>a</span>
-        <span>a</span>
-        <span>a</span>
+        {data.map((el) => (
+          <span key={el.name}>{el.name}</span>
+        ))}
       </ToyContainer>
     </Container>
   );
