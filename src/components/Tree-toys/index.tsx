1cb60d2a93a50e7b1cd189533511d05d50665b81
olesiklesha <olesik736@gmail.com>
2022-09-12 11:37:05 +0300

    feat: add base layout and styles to the toys block

--- src/components/Tree-toys/index.tsx
+++ src/components/Tree-toys/index.tsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import { Container, ToyContainer } from './styles';
+import { BlockTitle } from '../Filters-section/styles';
 
 const TreeToys = () => {
-  return <div>this is tree toys</div>;
+  return (
+    <Container>
+      <BlockTitle>Toys</BlockTitle>
+      <ToyContainer>
+        <span>a</span>
+        <span>a</span>
+        <span>a</span>
+      </ToyContainer>
+    </Container>
+  );
 };
 
 export default TreeToys;
---
+++ src/components/Tree-toys/styles.ts
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  width: 400px;
+  padding-top: 25px;
+  padding-left: 25px;
+`;
+
+export const ToyContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 10px;
+  width: 100%;
+`;
